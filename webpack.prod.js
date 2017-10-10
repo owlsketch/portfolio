2cb23499878180dc4900cb4c722a52d8de61ea97
owlsketch <satimidus@gmail.com>
2017-10-10 13:11:50 -0400

    Dealt with issue #13, decided on keeping float inline was too bothersome and float was already working just needed to update the constraints so that the content itself has a minimum and maximum width

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,15 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+const webpack = require('webpack');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
 	plugins: [
-		new UglifyJSPlugin()
+		new UglifyJSPlugin(),
+		//use to tell front-end component we are in production mode
+		new webpack.DefinePlugin({
+			'process.env': { 'NODE_ENV': JSON.stringify('production') }
+		})
 	]
 });
