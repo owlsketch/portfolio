74dac2fa5ec6324fd50a3feaa7f0006b18fff1bc
owlsketch <satimidus@gmail.com>
2017-10-20 02:17:12 +0000

    Revert "Included the gallery project"
    This reverts commit 4eaf07a098934d339686800701650d9161e4b908.

--- app/routes/index.routes.js
+++ app/routes/index.routes.js
@@ -6,18 +6,9 @@ module.exports = function(app) {
 
   app.get('/about', function(req, res) {
     res.render('pages/about.ejs');
-  });
+  })
 
-  //each project summary is a subroute of this route
-  //however the project itself has its own route.
-  //For example, the gallery is found at /gallery
-  //If a project has it's own domain name, then the
-  //route for that project redirects to that domain
   app.get('/projects', function(req, res) {
     res.render('pages/projects.ejs');
-  });
-
-  app.get('/gallery', function(req, res) {
-	res.render('projects/gallery.ejs');
-  });
+  })
 };
