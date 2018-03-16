3939fa5dc7e2c224c99e3aa7568ebb7112e2ebfd
owlsketch <satimidus@gmail.com>
2018-03-15 22:25:44 -0400

    cleaned up scss, improved routing - used mixins for linear gradients - 100% width instead of 100vw width   thus removing scrollbar issue
    - projects each have their own partial
    - / default loads pixel project
    - projects have their own routes
    - 404 handling for non-existing projects
    
    However, left/right arrows no longer
    function, want to dynamically load
    projects as AJAX requests instead.
    Will be done in next commit.

--- app/routes/index.routes.js
+++ app/routes/index.routes.js
@@ -1,11 +1,18 @@
 module.exports = function(app) {
   app.get('/', function(req, res) {
-    //set views to ./app/views
-    res.render('pages/projects.ejs');
+    //pass project var into template. Within 
+    //template use var to load pixel partial
+    res.render('pages/projects.ejs', {
+      project: 'pixel'
+    });
   });
 
-  app.get('/projects', function(req, res) {
-    res.render('pages/projects.ejs');
+  app.get('/projects/:project', function(req, res) {
+    //pass project var into template. Within 
+    //template use var to load :project partial
+    res.render('pages/projects.ejs', {
+      project: req.params.project
+    });
   })
 
   app.get('/about', function(req, res) {
