d717ca15b8c1ccada90e977525919725f5290f92
owlsketch <satimidus@gmail.com>
2018-03-16 23:13:48 -0400

    Async project load fully functional Issue had to do with losing the reference to our elements when they were changed by javascript. By updating references, the issue went away.
    I decided to not use classes to toggle
    display, opting instead to edit the display
    value as inline, reducing code and increasing
    support across browsers.
    
    Although async is fully functional, still need
    to update the href being displayed based on the
    project being displayed. Must support links!

--- public/js/projects/ArrowsHandler.js
+++ public/js/projects/ArrowsHandler.js
@@ -19,21 +19,24 @@ class ArrowsHandler {
     this.projectsContainer = document.getElementById("projects_container");
     this.activeIndex = this.getActiveIndex(this.projectsContainer);
   
-    this.prev.addEventListener('click', function(e) { state.clickedEvent(e, 'prev');});
-    this.next.addEventListener('click', function(e) { state.clickedEvent(e, 'next');});
+    this.prev.addEventListener('click', function(e) { state.clickedEvent(e, 'prev', state);});
+    this.next.addEventListener('click', function(e) { state.clickedEvent(e, 'next', state);});
 
-    document.addEventListener('keydown', function(e) { state.keypressEvent(e); });
+    document.addEventListener('keydown', function(e) { state.keypressEvent(e, state); });
   }
 
   getActiveIndex(container) {
     //only one project, but what's its index in our
     //internal script's list? (this.projects)
-    for(let i = 0; i < this.projects.length; i++) {
-      for(let j = 0; j < container.children.length; j++) {
-        let elem = container.children[j];
-        if(elem.classList.contains(this.projects[i].name)) {
-          this.projects[i].elem = elem;
-          return i; 
+    
+    //since ideally only one proj element exists, 
+    //easier to loop through projects list than elements
+    for(let i = 0; i < container.children.length; i++) {
+    let elem = container.children[i];
+      for(let j = 0; j < this.projects.length; j++) {
+        if(elem.classList.contains(this.projects[j].name)) {
+          this.projects[j].elem = elem;
+          return j; 
         }
       }
     }
@@ -42,12 +45,53 @@ class ArrowsHandler {
     return -1;
   }
 
-  clickedEvent(e, direction) {
-    let projectList = this.projects;
-    let originalElem = this.projects[this.activeIndex].elem;
-    let projectsContainer = this.projectsContainer;
+  keypressEvent(e, state) {
+    if(e.key === 'ArrowLeft') {
+      this.clickedEvent(e, 'prev', state);  
+    }
+    else if(e.key === 'ArrowRight') {
+      this.clickedEvent(e, 'next', state);
+    }
+  }
 
-    //change index by 1
+  clickedEvent(e, direction, ref) {
+    let state = ref;
+    let originalElem = state.projects[state.activeIndex].elem;
+    this.updateActiveIndex(direction);
+    
+    //if elem has been loaded already
+    if(state.projects[state.activeIndex].elem !== null) {
+      originalElem.style.display = 'none';
+      state.projects[state.activeIndex].elem.style.display = 'block';
+    }
+    else {
+      let httpRequest = new XMLHttpRequest();
+      httpRequest.onreadystatechange = alertContents;
+
+      httpRequest.open('GET', '/partial/' + state.projects[state.activeIndex].name);
+      httpRequest.send();
+      
+      function alertContents() {
+        if(httpRequest.readyState === XMLHttpRequest.DONE) {
+          if(httpRequest.status === 200) {
+            originalElem.style.display = 'none';
+            state.projectsContainer.innerHTML += httpRequest.responseText;
+
+            //my understanding is, because projectsContainer's innerHTML has
+            //changed, our reference to that element(and its children) is no
+            //longer valid. Thus we must update our var references if we wish
+            //to manipulate these elements in the future (we do)
+            state.updateReferences();
+          }
+          else {
+            alert("there was an error");
+          }
+        }
+      }
+    }
+  }
+
+  updateActiveIndex(direction) {
     if(direction === 'prev') {
       this.activeIndex = this.activeIndex - 1;
       if(this.activeIndex < 0) {
@@ -60,58 +104,21 @@ class ArrowsHandler {
         this.activeIndex = 0;
       }
     }
+  }
 
-    let activeIndex = this.activeIndex;
-    
-    if(this.projects[this.activeIndex].elem !== null) {
-      originalElem.style.display = 'none';
-      this.projects[this.activeIndex].elem.style.display = 'block';
-      /*
-      this.projects[this.activeIndex].elem.classList.remove('inactive');
-      this.projects[this.activeIndex].elem.classList.add('active');
-      originalElem.classList.remove('active');
-      originalElem.classList.add('inactive');
-      */
-    }
-    else {
-      let httpRequest = new XMLHttpRequest();
-      //funct to handle response
-      httpRequest.onreadystatechange = alertContents;
-
-      httpRequest.open('GET', '/partial/' + this.projects[this.activeIndex].name);
-      httpRequest.send();
-      
-      function alertContents() {
-        if(httpRequest.readyState === XMLHttpRequest.DONE) {
-          if(httpRequest.status === 200) {
-            console.log('loaded');
-            originalElem.style.display = 'none';
-            /*
-            originalElem.classList.remove('active');
-            originalElem.classList.add('inactive');
-            */
-            projectsContainer.innerHTML += httpRequest.responseText;
-
-            //finally add new elem to our internal list to indicate its been loaded
-            projectList[activeIndex].elem = document.getElementById(projectList[activeIndex].name);
-          }
-            else {
-            alert("there was an error");
-          }
+  updateReferences() {
+    this.projectsContainer = document.getElementById("projects_container");
+    //assign a proj element to an obj in projects list
+    for(let i = 0; i < this.projectsContainer.children.length; i++) {
+      let elem = this.projectsContainer.children[i];
+      for(let j = 0; j < this.projects.length; j++) {
+        if(elem.classList.contains(this.projects[j].name)) {
+          this.projects[j].elem = elem;
+          break;
         }
       }
     }
   }
-
-  keypressEvent(e) {
-    if(e.key === 'ArrowLeft') {
-      this.clickedEvent(e, 'prev');  
-    }
-    else if(e.key === 'ArrowRight') {
-      this.clickedEvent(e, 'next');
-    }
-  }
-
 };
 
 export default ArrowsHandler;
