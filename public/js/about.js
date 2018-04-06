517ddb272ac72360948bfc9c86b827723816ecef
owlsketch <satimidus@gmail.com>
2018-04-06 02:27:09 -0400

    Restyled about page(lavender), consistent design

--- public/js/about.js
+++ public/js/about.js
@@ -1,9 +1,2 @@
 import '../css/about.scss';
 import '../css/media_about.scss';
-
-//replace svg icons
-let gh_icon = document.getElementById("github_icon");
-let ig_icon = document.getElementById("instagram_icon");
-
-gh_icon.src = "../img/github_icon_blue.svg";
-ig_icon.src = "../img/instagram_icon_blue.svg";
