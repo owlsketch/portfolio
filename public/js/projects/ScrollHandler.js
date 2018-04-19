e41a4b195c88c3ac048407586237578d09bbda06
owlsketch <satimidus@gmail.com>
2018-04-19 19:49:36 -0400

    Updated scrolling code, img only fixed element Removed a lot of unnecessary css Only images are fixed on scrolling rewrote the scrolling code so it only triggers state changes once whenever a certain scrolling threshhold is reached. (MORE EFFICIENT!)

--- public/js/projects/ScrollHandler.js
+++ public/js/projects/ScrollHandler.js
@@ -2,11 +2,11 @@
 
 class ScrollHandler {
   constructor (elements) {
-    /** *** Set up *****/
+    /****** Set up *****/
     let state = this
+    let pos = this.getPos()
 
-    // retrieve elements to be manipulated through scroll
-    // attained height by: https://stackoverflow.com/a/18673641
+    /****** Retrieve el *****/
     this.nav = document.getElementById('nav_layer')
     this.navHeight = this.nav.getBoundingClientRect().top + document.documentElement.scrollTop
 
@@ -14,28 +14,50 @@ class ScrollHandler {
     // is updated whenever a new proj is loaded
     this.elements = elements
 
+    /******* Events ******/
     // is requestAnimationFrame optimization necessary?
     window.addEventListener('scroll', function (e) { state.scrollEvent(e) })
   }
 
   scrollEvent (e) {
-    // top nav has been passed
-    if (window.pageYOffset - this.navHeight > 0) {
-      this.nav.classList.add('nav_fixed')
-    } else {
-      this.nav.classList.remove('nav_fixed')
-    }
-    // have scrolled halfway through page
-    if (window.pageYOffset - window.innerHeight / 2 > 0) {
-      // for all projects, make their position absolute
-      for (let i = 0; i < this.elements.list.length; i++) {
-        this.elements.list[i].classList.add('project_main_absolute')
+    let currPos = this.getPos()
+    if (currPos !== this.pos) { 
+      this.pos = currPos
+      switch (currPos) {
+        case 0:
+          this.nav.classList.remove('nav_fixed')
+          break
+        case 1:
+          this.nav.classList.add('nav_fixed')
+          for (let i = 0; i < this.elements.list.length; i++) {
+            this.elements.list[i].children[0].classList.remove('text_none')
+          }
+          break
+        case 2:
+          for (let i = 0; i < this.elements.list.length; i++) {
+            this.elements.list[i].children[0].classList.add('text_none')
+          }
+          for (let i = 0; i < this.elements.list.length; i++) {
+            this.elements.list[i].children[1].classList.remove('project_img_absolute')
+          }
+          break
+        case 3:
+          for (let i = 0; i < this.elements.list.length; i++) {
+            this.elements.list[i].children[1].classList.add('project_img_absolute')
+          }
+        break
       }
-    } else {
-      for (let i = 0; i < this.elements.list.length; i++) {
-        this.elements.list[i].classList.remove('project_main_absolute')
-      }
     }
+  }
+  
+  getPos() {
+    let pos = 0
+    // 1: passed nav bar, 2: title text is covered, 3: scrolled halway through page
+    if (window.pageYOffset - this.navHeight > 0) pos = 1
+    if (window.pageYOffset - window.innerHeight / 3.6 > 0) pos = 2
+    if (window.pageYOffset - window.innerHeight / 2 > 0) pos = 3
+    
+    return pos;
   }
 };
 
