aaa81e94666ecf7f2ac703725733bfd4aa1bc4ce
owlsketch <satimidus@gmail.com>
2018-08-11 21:51:36 -0400

    Updated express to handle views via html

--- app/routes/index.routes.js
+++ app/routes/index.routes.js
@@ -1,18 +1,18 @@
-let path = require('path')
+const path = require('path')
 
 module.exports = function (app) {
+  const viewPath = app.get('viewPath')
+
   app.get('/', function (req, res) {
     res.redirect('projects/gallery')
   })
 
+  // [ UPDATE TO REACT ]
   app.get('/projects/:project', function (req, res) {
-    // pass project var into template. Within
-    // template use var to load :project partial
-    res.render('pages/projects.ejs', {
-      project: req.params.project
-    })
+    res.sendFile(path.join(viewPath, 'projects.html'))
   })
 
+  /* 
   app.get('/partial/:project', function (req, res) {
     // pass just the partial, an ajax request
     let project = req.params.project
@@ -28,8 +28,11 @@ module.exports = function (app) {
       root: path.join(__dirname, '../views/partials/articles')
     })
   })
+  // [ END UPDATE ]
+  */
+
 
   app.get('/about', function (req, res) {
-    res.render('pages/about.ejs')
+    res.sendFile(path.join(viewPath, 'about.html'))
   })
 }
