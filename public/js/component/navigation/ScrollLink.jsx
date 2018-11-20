5c74be1b5ec4a77a8d17ddb3dceae09c74ddd35f
owlsketch <satimidus@gmail.com>
2018-11-20 15:27:09 -0500

    Scroll link active in React

--- public/js/component/navigation/ScrollLink.jsx
+++ public/js/component/navigation/ScrollLink.jsx
@@ -2,15 +2,48 @@ import React from 'react'
 
 import './ScrollLink.scss'
 
-function ScrollLink () {
-  return (
-    <div className="project_scroll_link">
-      <div className="scroll_link">
-        <h3>MORE</h3>
-        <img width="48px" src="/img/landing/more_arrow.svg" alt="Read more" />
+class ScrollLink extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick (e) {
+    if ('requestAnimationFrame' in window === false) {
+      window.scroll(0, window.innerHeight / 2)
+    } else {
+      let startPos = window.pageYOffset
+      let startTime = 'now' in window.performance ? window.performance.now() : new Date().getTime()
+      let duration = 600
+
+      function scroll () {
+        let now = 'now' in window.performance ? window.performance.now() : new Date().getTime()
+        let time = Math.min(1, ((now - startTime) / duration))
+
+        window.scroll(
+          0,
+          Math.ceil(((time * (2 - time)) * ((window.innerHeight / 2) - startPos)) + startPos)
+        )
+
+        if (window.pageYOffset >= window.innerHeight / 2) { return }
+        window.requestAnimationFrame(scroll)
+      }
+
+      scroll()
+    }
+  }
+
+  render () {
+    return (
+      <div className="project_scroll_link" onClick={this.handleClick}>
+        <div className="scroll_link">
+          <h3>MORE</h3>
+          <img width="48px" src="/img/landing/more_arrow.svg" alt="Read more" />
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default ScrollLink
--- public/js/projects.js
+++ public/js/projects.js
@@ -5,8 +5,10 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './component/App'
 
-// want media queries to implement on React components and normal
-// elements AFTER all of their own unique stylings have been applied
+/*
+want media queries to implement on React components and normal
+elements AFTER all of their own unique stylings have been applied
+*/
 import '../css/media_projects.scss'
 
 ReactDOM.render(
