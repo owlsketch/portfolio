79ec3d7cc2d9949ce8db0bab1fd8a27363ac9540
owlsketch <satimidus@gmail.com>
2018-12-14 04:34:19 -0500

    Created article content

--- public/js/component/content/Gallery.jsx
+++ public/js/component/content/Gallery.jsx
@@ -9,25 +9,25 @@ function galleryImage (fixed) {
 
       <div className="layer gallery_layer_bottom">
         <div className="gallery_portrait">
-          <img className="img_gallery_landscape scroll_link" src="/img/landing/gallery/landscape_left.jpg" width="288px" alt="Claude Monet landscape painting"/>
+          <img className="img_gallery_landscape scroll_link" src="/img/landing/gallery/landscape_left.jpg" width="288px"/>
         </div>
         <div className="gallery_portrait">
-          <img className="img_gallery_landscape scroll_link" src="/img/landing/gallery/portrait_left.jpg" width="288px" alt="Claude Monet portrait painting"/>
+          <img className="img_gallery_landscape scroll_link" src="/img/landing/gallery/portrait_left.jpg" width="288px"/>
         </div>
       </div>
 
       <div className="layer gallery_layer_middle">
         <div className="gallery_portrait">
-          <img className="img_gallery_portrait scroll_link" src="/img/landing/gallery/portrait_right.jpg" width="172px" alt="Gallery main menu modal"/>
+          <img className="img_gallery_portrait scroll_link" src="/img/landing/gallery/portrait_right.jpg" width="172px"/>
         </div>
         <div className="gallery_portrait">
-          <img className="img_gallery_portrait scroll_link" src="/img/landing/gallery/landscape_right.jpg" width="172px" alt="Claude Monet portrait painting"/>
+          <img className="img_gallery_portrait scroll_link" src="/img/landing/gallery/landscape_right.jpg" width="172px"/>
         </div>
       </div>
 
       <div className="layer gallery_layer_top">
         <div className="gallery_modal">
-          <img className="img_gallery_modal scroll_link" src="/img/landing/gallery/gallery_modal.png" width="272px" alt="Claude Monet landscape painting"/>
+          <img className="img_gallery_modal scroll_link" src="/img/landing/gallery/gallery_modal.png" width="272px"/>
         </div>
       </div>
 
@@ -37,29 +37,75 @@ function galleryImage (fixed) {
 
 function galleryArticle () {
   return (
-    <div className="proj_article gallery_article">
+    <div className="gallery_article">
 
-      <div className="proj_description">
-        <p>
-          Gallery is an interactive 3D art gallery on the web.
-          Its goal is to allow visitors to experience art on the internet in an
-          immersive and approachable manner without the need of expensive VR equipment.
-        </p>
-      </div>
       <div className="proj_links">
         <a href="https://owlsketch.com/gallery">Visit Gallery</a>
         <a href="https://github.com/owlsketch/gallery">View Code</a>
       </div>
-      <div className="gallery_article_image">
-        <img className="gallery_img_claude_angle" width="1000px" src="/img/article/gallery/ClaudeAngle.jpg" alt="Claude Monet Gallery main presentation"/>
-      </div>
-      <div className="gallery_article_image">
-        <div className="gallery_img_flex">
-          <img width="460px" src="/img/article/gallery/ClaudeGroup.jpg" alt="Claude Monet Group of Artwork"/>
-          <img width="460px" src="/img/article/gallery/ClaudeTitle.jpg" alt="Claude Monet Gallery main menu"/>
-        </div>
-      </div>
 
+      <h2 className="article_title">Immersive Art Galleries for the Web</h2>
+      <article className="article_content">
+        <p>
+          Laika Gallery, as the name hints at, is an effort to bring the experience of art galleries to the web. Fundamentally, at its core also lies the concept of accessibility.
+        </p>
+        <p>
+          The project began after I read about the Metropolitan Museum's Open Access Initiative, an effort to publish the museum's collection of public-domain artworks online. The Met's efforts succeeded in making information available to the public. However, they failed to make it as consumable as an actual visit to a museum. 
+        </p>
+
+        <div className="article_img">
+          <img src="/img/article/gallery/article_img_1.JPG"/>
+          <p className="subtext">
+            Results from exploring the Met’s Open Access Initiative.
+          </p>
+        </div>
+
+        <p>
+          When you go to a museum, the experience is a curated one. The interaction between the space, the arrangement, and the pieces conveys a unique experience. As the visitor, your first impression is the gallery as a whole. Unfortunately, that interaction gets lost on the internet. The space is reduced to a two-dimmensional plane, losing its depth. The arrangement becomes a pre-configured list designed to handle an infinite amount of pieces, but without a single afterthought on the possible associations amongst them. The pieces themselves become isolated and lose their sense of scale.
+        </p>
+
+        <p className="article_quote">
+          The interaction between the space, the arrangement, and the pieces conveys a unique experience. 
+        </p>
+  
+        <p>
+          As a response to these problems, I created the first working version of Laika Gallery in 2014. The goal was to bring all that lost information back, or at least as much as I could get away with. The first version was also for my grandmother; I wanted to be able to bring the museum to her since it proved difficult to do the contrary. With this having been accomplished, I shared the code on Github and went back to focusing on my studies.
+        </p>
+  
+        <div className="article_img">
+          <img src="/img/article/gallery/article_img_2.jpg"/>
+          <p className="subtext">
+            The main menu from the first version of Laika Gallery.
+          </p>
+        </div>
+
+        <p>
+          Years later a couple of events sparked my interest in this project again.
+        </p>
+        <p>
+          I received a few messages online from artists thanking me for the code I had written, as they had repurposed the code to showcase their own artwork. This project originally had the purpose of making the Met's Open Collection more accessible and consumable online. However, these messages made me realize that the concept of accessibility could be further developed to transform this project into a tool that could be used by any artist to create an immersive and interactive experience on the web. 
+        </p>
+
+        <p>
+          The other event that further inspired me was a demo I saw for a VR museum experience by the Smithsonian. It was beautifully rendered, and definitely something I couldn't compete with. Yet, with accessibility in mind, what I saw had no real audience. The cost of running the application involved a VR headset and a computer strong enough to render the view. It was developed with a unique rig by Intel running the latest specs and in the process it was forgotten that the average user doesn't have anything near comparable to that. Related projects that already exist on the web run into similar problems by relying on Unity and thus reducing their accessibility. In these scenarios, the technical cost of the application becomes a burden on the user that results in turning many away.
+        </p>
+        
+        <div className="article_img">
+          <img src="/img/article/gallery/article_img_3.jpg"/>
+          <p className="subtext">
+            We began developing the newest version of Laika Gallery in July 2018.
+          </p>
+        </div>
+
+        <p>
+          So I decided to work on Laika Gallery once again, this time as a collaborative effort, with accessibility still in mind. We're aiming to create a tool that makes gallery creation accessible to artists while minimizing the technical cost to visitors. To make this a reality means consciously working well within the bounds of obsolete hardware just like the Nintendo Switch managed to accomplish. Laika Gallery can't promise hyper-realistic content, but by decoupling the product from bleeding-edge technology, what can be promised is access for a wider audience. By not defaulting towards Unity, as many of our competitors have done, and working with Three.js instead, the technical cost for users is greatly reduced, thus increasing accessibility. This does come at the cost of prolonged development time, however I don't see this as a setback, but as a necessity to getting it done right. 
+        </p>
+        
+        <p>
+          With all of this in mind, we're hoping to have the first release happen in February 2019. 
+        </p>
+        <img className="endmark" width="48px" src="/img/article/owlsketch_black.png"/>
+      </article>
     </div>
   )
 }
--- public/js/component/content/Pixel.jsx
+++ public/js/component/content/Pixel.jsx
@@ -6,34 +6,59 @@ function pixelImage (fixed) {
 
   return (
     <div className={imgClasses}>
-      <img className="img_pixel_wall" width="525px" src="/img/landing/pixel/pixelwall.png" alt="10x10 pixel wall device" />
+      <img className="img_pixel_wall" width="525px" src="/img/landing/pixel/pixelwall.png"/>
     </div>
   )
 }
 
 function pixelArticle () {
   return (
-    <div className="proj_article pixel_article">
-
-      <div className="proj_description">
-        <p>
-          Made for GRID, the Pixel Wall is an interactive LED Display designed
-          to welcome students to a space that both promotes participation with
-          technology and provides resources for students to learn and create.
-        </p>
-      </div>
+    <div className="pixel_article">
 
       <div className="proj_links">
         <a href="https://owlsketch.com/pixel">Visit Pixel Wall</a>
         <a href="https://github.com/RutgersGRID/pixelwall">View Code</a>
       </div>
-      <div className="pixel_mobile_imgs">
-        <img className="pixel_portrait1" width="244px" src="/img/article/pixel/mob_portrait1.PNG" alt="10x10 pixel drawing of a heart"/>
-        <img className="pixel_portrait2" width="244px" src="/img/article/pixel/mob_portrait2.png" alt="32x16 pixel drawing of Mr.Krabs shocked eyes"/>
-        <img className="pixel_landscape1" width="500px" src="/img/article/pixel/mob_landscape1.PNG" alt="32x16 pixel drawing of random colored scribbles"/>
-        <img className="pixel_landscape2" width="500px" src="/img/article/pixel/mob_landscape2.png" alt="32x16 pixel drawing of deer in a field"/>
-      </div>
+    
+      <h2 className="article_title">Interactive ‘Internet Of Things’ LEDs</h2>
+      <article className="article_content">
+        <p>
+          The Pixel Wall project consists of an interactive LED grid device along with software to control the device through the web. It was designed for GRID so that visitors may feel welcome to a space that both promotes participation with technology, and provides resources for others to learn and create.
+        </p>
 
+        <p>
+          The device consists of a grid of LEDs, a microcontroller, and a host computer. The grid of LEDs are controlled by the microcontroller that in turn receives commands from the computer it is connected to. In order to allow the computer to communicate with the microcontroller that controls the LEDs while it is running, we loaded the Firmata protocol onto the microcontroller. With this in place, a basic set of commands were defined that would allow the microcontroller to control the LEDs when these commands are sent by the host computer. 
+        </p>
+
+        <p className="article_quote">
+          The experience we wanted visitors to have was one where they can instantly control an element in their environment from their phones.
+        </p>
+
+        <p>
+          In order to allow the device to be controlled through the internet, I made use of the socket.io library. The device, once set up and running, automatically connects to a predetermined socket. I then created a site which gives any visitor access to the same socket. By having vistors access the site, they can send commands through the site's interface, which are then received by the host computer of the device and sent as commands to the microcontroller.
+        </p>
+
+        <p>
+          The interface of the site was designed with a mobile-first approach. The experience we wanted visitors to have was one where they can instantly control an element in their environment from their phones.
+        </p>
+
+        <p>
+          By the end of my contract with GRID we managed to create a handful of prototypes, one of which was featured by Mozilla's Internet Of Things team at MakerFaire in California. Before leaving, we also managed to constuct a pixel wall with a total of 2,700 LEDs, with help from a Make article I found online.
+        </p>
+
+        <p>
+          After having graduated from Rutgers, this was my first 'real' project. It was my first opportunity to prove that I really had the skills to create with what I had been learning over the years. Nevertheless, I couldn’t have managed to pursue this project without the help from my friends at GRID. This project also helped show me the direction I need to be heading towards in order to live a meaningful life.
+        </p>
+
+        <p>
+          I hope to one day be able to visit this project again. I accomplished many of the technical goals that were set out for me, however I believe there were many social shortcomings. A greater effort could have been made to entice people into making their own pixel walls. That initiative could have been provoked through the use of a common microcontroller during development that is more easily accessible than the one that we did utilize. Another shortcoming was the lack of a formalized set of instructions for creating a pixel wall. In particular, the assembly of the container, which I had very little knowledge on how to approach myself admittedly. 
+        </p>
+
+        <p>
+          Hopefully one day I can build upon what is already in place and create a truly open source device, that also succeeds in allowing makers to create their own devices and to learn along the journey. Regardless, I'm proud of having built a device that entertained lots of people and had them interested in how I made it happen.
+        </p>
+        <img className="endmark" width="48px" src="/img/article/owlsketch_black.png"/>
+      </article>
     </div>
   )
 }
