6ec476f70c0f76114c6b0378300be5e0c6525469
owlsketch <os.ggarcia16@gmail.com>
2022-01-10 13:41:48 -0500

    Finished updating across page

--- client/app/components/content/across.jsx
+++ client/app/components/content/across.jsx
@@ -17,8 +17,49 @@ function acrossImage (fixed) {
 function acrossArticle () {
   return (
     <div className='across_article'>
-      <h2 className='article_title'>Across La Tierra: A VR Narrative</h2>
+      <h2 className='article_title'>A VR Narrative on Immigrant Dreams</h2>
       <article className='article_content'>
+        <p>
+          Across la Tierra is an interactive virtual reality experience that mixes 3D environments, 360° video, interviews and original music in order to create an immersive environment for the viewer to explore. It was directed and produced by Layqa Nuna Yawar, produced and programmed by Gibran Garcia, with an original score by Yeimy Gamez Castillo.
+        </p>
+
+        <div className='article_video across_videoContainer'>
+          <iframe
+            className='across_video'
+            src="https://player.vimeo.com/video/465998862?h=d99eb95de2&autoplay=1&portrait=0&muted=1"
+            width="576"
+            height="324"
+            frameBorder="0"
+            allow="autoplay; fullscreen; picture-in-picture"
+            allowFullScreen>
+          </iframe>
+        </div>
+
+        <p>
+          Objects found along this journey are interactive and can be picked up. When held, these objects trigger 360° video interviews and landscape captures that start to weave multiple narratives together. A framed picture of the artist’s grandmother takes you to a cemetery in Ecuador to visit her grave. Some are memories of the artist’s own family history, while other objects tell somebody else’s immigrant story, perspective or dreams.
+        </p>
+
+        <div className='article_video across_float'>
+          <video autoPlay loop className='across_video'>
+            <source type='video/webm' src='/assets/img/article/across/articleVideo1.webm'/>
+          </video>
+        </div>
+
+        <p>
+          It was funded by an Open Society Foundation Fellowship and was based on a personal investigation of the artist&apos;s family migration history. This project takes place between Ecuador in South America, New York City in North America and is connected by a virtual space and the space between continents and immigrant dreams.
+        </p>
+
+        <p>
+          Keeping in mind that new technologies are often not accessible to communities of color and migrant populations, Across la Tierra aims to bridge mural making and emergent digital technologies by becoming an experiment in storytelling and community engaged practices.
+        </p>
+
+        <div className='article_img across_flyer'>
+          <img src='/assets/img/article/across/articleImg1.jpg'/>
+          <p className='subtext'>
+            Flyer for first public viewing at the Newark Arts Festival.
+          </p>
+        </div>
+
         <img className='endmark' width='48px' src='/assets/img/article/owlsketchBlack.png'/>
       </article>
     </div>
--- client/app/components/content/gallery.jsx
+++ client/app/components/content/gallery.jsx
@@ -44,21 +44,18 @@ function galleryArticle () {
       <h2 className='article_title'>Immersive Art Galleries for the Web</h2>
       <article className='article_content'>
         <p>
-          <a href='https://www.laika.gallery'>Laika Gallery</a> is an effort to bring the experience of art galleries to the web. At its core also lies the concept of accessibility.
-        </p>
-        <p>
-          The project began thanks to the Metropolitan Museum&#39;s <a href='https://metmuseum.org/blogs/digital-underground/2017/open-access-at-the-met'>Open Access Initiative</a>, a project publishing the museum&#39;s collection of public-domain artworks online. The project makes artwork available to the public, however, it fails to be as consumable as an actual visit to the museum.
+          <a href='https://www.laika.gallery'>Laika Gallery</a> is a website that allows artists to create 3D art exhibitions in order to promote and sell their artwork online. It&apos;s available for both mobile and desktop and doesn&apos;t require a third-party plug-in to function.
         </p>
 
         <div className='article_img'>
-          <img src='/assets/img/article/gallery/articleImg1.jpg'/>
+          <img src='/assets/img/article/gallery/articleImg2.jpg'/>
           <p className='subtext'>
-            Results from exploring the Met’s Open Access Initiative.
+            A gallery made using Laika Gallery.
           </p>
         </div>
 
         <p>
-          Going to a museum is a curated experience involving the interaction between the pieces, their arrangement, and the space. First impressions are about the gallery as a whole, something that is lost when using the internet as a medium. Pieces are isolated and their sense of scale is lost. Their arrangement isn&#39;t curated but programatically preconfigured. The space is reduced to a two-dimmensional plane.
+          The project began thanks to the Metropolitan Museum&#39;s <a href='https://metmuseum.org/blogs/digital-underground/2017/open-access-at-the-met'>Open Access Initiative</a>, a project publishing the museum&#39;s collection of public-domain artworks online. The project makes artwork available to the public, however, it fails to be as consumable as an actual visit to the museum which is where Laika Gallery steps in.
         </p>
 
         <p className='article_quote'>
@@ -66,38 +63,27 @@ function galleryArticle () {
         </p>
 
         <p>
-          As a response to these issues, a prototype of Laika Gallery was created in 2014. The first version was also for my grandmother; I wanted to be able to bring the museum to her since it was difficult for us to travel to the Met.
+          From there, the project was repurposed to showcase anyone&apos;s artwork.  By transforming this project into a tool that could be used by any artist to create an immersive and interactive experience on the web, the concept of accessibility came to the forefront of our development. The aim was to create a tool that makes gallery creation accessible to artists while minimizing the technical cost to the viewer.
         </p>
 
         <div className='article_img'>
-          <img src='/assets/img/article/gallery/articleImg2.jpg'/>
+          <img src='/assets/img/article/gallery/articleImg1.jpg'/>
           <p className='subtext'>
-            A preview from the first version of Laika Gallery.
+            Easy management of all your works through the user page.
           </p>
         </div>
 
         <p>
-          This project originally intended to make the Met&#39;s Open Collection more accessible online. However, I received a few messages online from artists over the years thanking me for the code I had written, as they had repurposed the code to showcase their own artwork. I realized that the concept of accessibility could be further developed to transform this project into a tool that could be used by any artist to create an immersive and interactive experience on the web.
-        </p>
-
-        <p>
-          Another cause for developing Laika Gallery further was a demo I tried out for a <a href='https://www.engadget.com/2017/08/02/smithsonian-art-museum-intel-vr/'>VR museum experience by the Smithsonian</a>. The experience was just like being at the museum itself! Yet, with accessibility in mind, what I tried out had no real or significant audience. The cost of running the application involved a VR headset and a computer strong enough to render the experience. It was developed with a unique rig by Intel running the latest specs and in that process it was forgotten that the average user doesn&#39;t have anything near comparable to that. Likewise, similar projects that already exist on the web run into similar problems by relying on Unity and thus reducing their accessibility. In these scenarios, the technical cost of the application becomes a burden on the user that results in turning many away.
+          To make this a reality meant consciously working well within the bounds of <a href='https://www.forbes.com/sites/davidthier/2018/02/05/the-nintendo-switchs-secret-is-that-its-already-obsolete/#e5e745539136'>obsolete hardware just like the Nintendo Switch managed to accomplish</a>. Laika Gallery can&#39;t promise hyper-realistic content, but by decoupling the product from bleeding-edge technology, what can be delivered is access for a wider audience. By not defaulting towards Unity, as many competitors have done, and using Three.js instead, the technical cost is greatly reduced, thus increasing accessibility.
         </p>
 
         <div className='article_img'>
           <img src='/assets/img/article/gallery/articleImg3.jpg'/>
           <p className='subtext'>
-            We began developing the newest version of Laika Gallery in July 2018.
+            A seamless editor that works on both mobile and desktop.
           </p>
         </div>
 
-        <p>
-          So I decided to work on Laika Gallery once again with accessibility still in mind. I&#39;m aiming to create a tool that makes gallery creation accessible to artists while minimizing the technical cost to the viewer. To make this a reality means consciously working well within the bounds of <a href='https://www.forbes.com/sites/davidthier/2018/02/05/the-nintendo-switchs-secret-is-that-its-already-obsolete/#e5e745539136'>obsolete hardware just like the Nintendo Switch managed to accomplish</a>. Laika Gallery can&#39;t promise hyper-realistic content, but by decoupling the product from bleeding-edge technology, what can be delivered is access for a wider audience. By not defaulting towards Unity, as many competitors have done, and using Three.js instead, the technical cost for users is greatly reduced, thus increasing accessibility. This does come at the cost of prolonged development time, however I don&#39;t see this as a setback, but as a necessity to getting it done right.
-        </p>
-
-        <p>
-          With all of this in mind, I&#39;m hoping to have the first release happen in July 2021!
-        </p>
         <img className='endmark' width='48px' src='/assets/img/article/owlsketchBlack.png'/>
       </article>
     </div>
